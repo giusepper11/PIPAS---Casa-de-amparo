3c06f5afbda09e14f08de3f43eb540c2c868312f
Giuseppe Rosa <giusepper11@gmail.com>
2018-11-07 21:00:15 +0000

    Adicionado model lista de instituições

--- casa_amparo/settings.py
+++ casa_amparo/settings.py
@@ -44,9 +44,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
     'home',
     'new_features',
-    'accounts'
+    'instituicoes',
 ]
 
 MIDDLEWARE = [
---
+++ instituicoes/models.py
@@ -0,0 +1,41 @@
+from django.db import models
+from accounts.models import Instituicoes
+# Create your models here.
+
+
+class InstituicaoLista(models.Model):
+    id = models.IntegerField(primary_key=True)
+    instituicao = models.CharField(max_length=255, blank=True, null=True)
+    bairro = models.CharField(max_length=255, blank=True, null=True)
+    cep = models.CharField(max_length=255, blank=True, null=True)
+    cidade = models.CharField(max_length=255, blank=True, null=True)
+    descricao = models.TextField(max_length=1000, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
+    endereco = models.CharField(max_length=255, blank=True, null=True)
+    entidade_que_administra_o_abrigo = models.CharField(max_length=255, blank=True, null=True)
+    estado = models.CharField(max_length=255, blank=True, null=True)
+    facebook = models.CharField(max_length=255, blank=True, null=True)
+    idade = models.CharField(max_length=255, blank=True, null=True)
+    numero_de_criancas = models.CharField(max_length=255, blank=True, null=True)
+    pais = models.CharField(max_length=255, blank=True, null=True)
+    possui_programa_de_apadrinhamento_afetivo = models.CharField(max_length=255, blank=True, null=True)
+    responsavel = models.CharField(max_length=255, blank=True, null=True)
+    site = models.CharField(max_length=255, blank=True, null=True)
+    telefone = models.CharField(max_length=255, blank=True, null=True)
+    end_format = models.CharField(max_length=255, blank=True, null=True)
+    lat_long = models.CharField(max_length=255, blank=True, null=True)
+    user_inst = models.ForeignKey(Instituicoes, blank=True, null=True, on_delete=models.DO_NOTHING)
+    logo_img = models.ImageField(blank=True, null=True)
+
+    def __str__(self):
+        try:
+            return self.instituicao.capitalize()
+        except:
+            return 'instutição sem nome'
+
+    def get_lat_long(self):
+        if self.lat_long:
+            long_lat = self.lat_long.split(',')
+            return {'lng': long_lat[-1], 'lat': long_lat[0]}
+        else:
+            return None
