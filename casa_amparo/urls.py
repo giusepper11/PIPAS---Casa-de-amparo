5d55b542bad581f8851170e2e15f19cccf4f2d0f
Giuseppe Rosa <giusepper11@gmail.com>
2018-11-07 22:12:03 +0000

    Never cache added

--- casa_amparo/urls.py
+++ casa_amparo/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.decorators.cache import never_cache
+
 
 from accounts.views import pessoa_fisica
 from home.views import SignupView
@@ -10,8 +12,8 @@ urlpatterns = [
                   path('', include('home.urls')),
                   path('new_features/', include('new_features.urls')),
                   path('accounts/', include('django.contrib.auth.urls')),
-                  path('accounts/signup/', SignupView.as_view(), name='cadastro_select'),
-                  path('accounts/signup/pf', pessoa_fisica.PessoaFisicaSignUpView.as_view(), name='cadastro_pf'),
+                  path('accounts/signup/', never_cache(SignupView.as_view()), name='cadastro_select'),
+                  path('accounts/signup/pf', never_cache(pessoa_fisica.PessoaFisicaSignUpView.as_view()), name='cadastro_pf'),
                   # path('accounts/signup/pj', ),
                   # path('accounts/signup/instituicao', ),
                   path('admin/', admin.site.urls),
--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from home.views import IndexView
+from django.views.decorators.cache import never_cache
+
 
 app_name = 'home'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index'),
+    path('', never_cache(IndexView.as_view()), name='index'),
 ]
