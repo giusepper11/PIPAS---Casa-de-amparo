3fae86237faa5e6523ceca935528f57e494dee94
Giuseppe Rosa <giusepper11@gmail.com>
2018-11-07 20:11:23 +0000

    Adicionado model lista de instituições

--- accounts/models.py
+++ accounts/models.py
@@ -72,3 +72,45 @@ class Mensagens(models.Model):
 
     def __str__(self):
         return f'{self.remetente} - {self.destinatario}'
+
+
+class InstituicaoLista(models.Model):
+    id = models.IntegerField(primary_key=True)
+    instituicao = models.CharField(max_length=255, blank=True, null=True)
+    bairro = models.CharField(max_length=255, blank=True, null=True)
+    cep = models.CharField(max_length=255, blank=True, null=True)
+    cidade = models.CharField(max_length=255, blank=True, null=True)
+    descricao = models.CharField(max_length=255, blank=True, null=True)
+    email = models.CharField(max_length=255, blank=True, null=True)
+    endereco = models.CharField(max_length=255, blank=True, null=True)
+    entidade_que_administra_o_abrigo = models.CharField(max_length=255, blank=True, null=True)
+    estado = models.CharField(max_length=255, blank=True, null=True)
+    facebook = models.CharField(max_length=255, blank=True, null=True)
+    idade = models.CharField(max_length=255, blank=True, null=True)
+    numero_de_criancas = models.CharField(max_length=255, blank=True, null=True)
+    pais = models.CharField(max_length=255, blank=True, null=True)
+    possui_programa_de_apadrinhamento_afetivo = models.CharField(max_length=255, blank=True, null=True)
+    responsavel = models.CharField(max_length=255, blank=True, null=True)
+    site = models.CharField(max_length=255, blank=True, null=True)
+    telefone = models.CharField(max_length=255, blank=True, null=True)
+    end_format = models.CharField(max_length=255, blank=True, null=True)
+    lat_long = models.CharField(max_length=255, blank=True, null=True)
+    user_inst = models.ForeignKey(Instituicoes, blank=True, null=True, on_delete=models.DO_NOTHING)
+    logo_img = models.ImageField(blank=True, null=True)
+
+    class Meta:
+        managed = True
+        db_table = 'instituicao_lista'
+
+    def __str__(self):
+        try:
+            return self.instituicao.capitalize()
+        except:
+            return 'instutição sem nome'
+
+    def get_lat_long(self):
+        if self.lat_long:
+            long_lat = self.lat_long.split(',')
+            return {'lng': long_lat[-1], 'lat': long_lat[0]}
+        else:
+            return None
