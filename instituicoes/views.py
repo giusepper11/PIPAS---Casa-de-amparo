23675526e58bd627459b71c8b90bb928915a87be
Giuseppe Rosa <giusepper11@gmail.com>
2018-11-14 11:03:51 +0000

    Nova pagina lista de instituições

--- instituicoes/views.py
+++ instituicoes/views.py
@@ -14,6 +14,12 @@ class InstituicoesListView(ListView):
     def get_queryset(self):
         return self.model.objects.exclude(instituicao__isnull=True).exclude(instituicao__exact='').order_by('bairro')
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['bairros'] = InstituicaoLista.objects.order_by().values('bairro').distinct()
+        return context
+
+
 
 class BairrosListView(TemplateView):
     template_name = 'instituicoes/lista_inst.html'
