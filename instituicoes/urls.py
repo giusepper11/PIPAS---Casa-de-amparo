0ded1e6b01c75421cbaf5d8659c39f97defd7343
Giuseppe Rosa <giusepper11@gmail.com>
2018-11-15 13:33:32 +0000

    Adicionado filtro AJAX para instituição por bairro

--- instituicoes/urls.py
+++ instituicoes/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
-from instituicoes.views import InstituicoesListView
+from instituicoes.views import InstituicoesListView, ajax_instfilter
 from django.views.decorators.cache import never_cache
 
 
 app_name = 'instituicoes'
 
 urlpatterns = [
-    path('', never_cache(InstituicoesListView.as_view()), name='lista_bairros'),
+    path('', InstituicoesListView.as_view(), name='lista_bairros'),
+
+    path('inst_filter/', ajax_instfilter, name='ajax_instfilter'),
 ]
