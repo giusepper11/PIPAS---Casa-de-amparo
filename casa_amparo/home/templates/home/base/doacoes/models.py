1ebd551b00fa0a6a2c5af86c2ecdbc842d1162a1
ybarros7 <yagodebarros@hotmail.com>
2019-04-12 01:15:18 +0000

    Atualização da Home e remoção de objetos do rodape

---
+++ casa_amparo/home/templates/home/base/doacoes/models.py
@@ -0,0 +1,37 @@
+# from django.db import models
+# from casa_amparo.instituicoes.models import InstituicaoLista
+# from casa_amparo.users.models import Pessoa
+#
+#
+# class DemandaDoacao(models.Model):
+#     instituicao = models.ForeignKey(InstituicaoLista, on_delete=models.DO_NOTHING)
+#     doacao = models.CharField(max_length=200)
+#     qtd = models.IntegerField(default=1)
+#     obs = models.TextField(blank=True, null=True)
+#     recebido = models.BooleanField(default=False)
+#     dt_criacao = models.DateTimeField(auto_now_add=True)
+#     dt_atualizacao = models.DateTimeField(auto_now=True)
+#
+#
+# class Doacao(models.Model):
+#     doador = models.ForeignKey(Pessoa, on_delete=models.DO_NOTHING)
+#     demanda = models.ForeignKey(DemandaDoacao, on_delete=models.DO_NOTHING)
+#     dt_criacao = models.DateTimeField(auto_now_add=True)
+#     dt_atualizacao = models.DateTimeField(auto_now=True)
+#     obs = models.TextField(blank=True, null=True)
+#
+#
+# class DoacaoOutros(models.Model):
+#     TIPO_DOACAO = (
+#         ('MONEY', 'Dinheiro'),
+#         ('PRODUCTS', 'Produtos'),
+#
+#         ('SERVICES', 'Serviços')
+#     )
+#
+#     doador = models.ForeignKey(Pessoa, on_delete=models.DO_NOTHING)
+#     instituicao = models.ForeignKey(InstituicaoLista, on_delete=models.DO_NOTHING)
+#     tipo_doacao = models.CharField(max_length=8, choices=TIPO_DOACAO)
+#     doacao = models.CharField(max_length=300)
+#     dt_criacao = models.DateTimeField(auto_now_add=True)
+#     dt_atualizacao = models.DateTimeField(auto_now=True)
