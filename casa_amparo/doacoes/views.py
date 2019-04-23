6d0bd21242be55122637a19179f080af02e88e28
Giuseppe Rosa <giusepper11@gmail.com>
2019-04-22 22:21:33 -0300

    implementando painel de doação

--- casa_amparo/doacoes/views.py
+++ casa_amparo/doacoes/views.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.utils.decorators import method_decorator
 from django.views.generic import CreateView, DetailView, ListView
 
 from casa_amparo.doacoes.forms import DoacaoOutrosForm
 from casa_amparo.doacoes.models import DemandaDoacao
-
 # Create your views here.
 from casa_amparo.users.decorators import instituicao_required
 
@@ -32,3 +30,8 @@ class DoacaoDashboardView(ListView):
 
     def get_queryset(self):
         return self.model.objects.filter(instituicao__user_inst__pf__user__id=self.request.user.id)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['body_style'] = 'profile-page'
+        return context
