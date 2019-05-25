2a2345bcb31344eddcbd977de61ac3366b620fdb
Giuseppe Rosa <giusepper11@gmail.com>
2019-05-25 10:59:38 -0300

    Adicionando relatorio de usuarios

--- casa_amparo/users/admin.py
+++ casa_amparo/users/admin.py
@@ -1,9 +1,43 @@
 from django.contrib import admin
 from casa_amparo.users.models import *
 
+
 #
 
-admin.site.register(CustomUser)
+class UsuarioListaAdmin(admin.ModelAdmin):
+    list_display = ('email', 'username', 'date_joined', 'last_login', 'eh_pf', 'eh_pj', 'esta_ativo')
+    search_fields = ('email', 'username')
+    list_filter = ('is_pf', 'is_pj', 'is_active')
+
+    def esta_ativo(self, obj):
+        if obj.is_active:
+            return True
+        else:
+            return False
+
+    esta_ativo.short_description = 'Ativo'
+    esta_ativo.boolean = True
+
+    def eh_pf(self, obj):
+        if obj.is_pf:
+            return True
+        else:
+            return False
+
+    eh_pf.short_description = 'Pessoa Fisica'
+    eh_pf.boolean = True
+
+    def eh_pj(self, obj):
+        if obj.is_pj:
+            return True
+        else:
+            return False
+
+    eh_pj.short_description = 'Pessoa Juridica'
+    eh_pj.boolean = True
+
+
+admin.site.register(CustomUser, UsuarioListaAdmin)
 admin.site.register(PessoaJuridica)
 admin.site.register(PessoaFisica)
 admin.site.register(Instituicoes)
