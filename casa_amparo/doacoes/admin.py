7f47196eab4f133fcbafe948bec18c08f7edc020
Giuseppe Rosa <giusepper11@gmail.com>
2019-05-25 11:46:46 -0300

    Adicionando relatório de demanda doação

--- casa_amparo/doacoes/admin.py
+++ casa_amparo/doacoes/admin.py
@@ -14,6 +14,27 @@ class DoacaoOutrosListaAdmin(admin.ModelAdmin):
     get_instituicao.admin_order_field = 'instituicao__instituicao'
 
 
-admin.site.register(DemandaDoacao)
+class DemandaDoacaoListAdmin(admin.ModelAdmin):
+    list_display = ('get_instituicao', 'doacao', 'qtd', 'obs', 'foi_recebido', 'dt_criacao')
+    search_fields = ('instituicao__instituicao', 'doacao')
+    list_filter = ('doacao', 'recebido')
+
+    def get_instituicao(self, obj):
+        return obj.instituicao.instituicao
+
+    get_instituicao.short_description = 'Instituição'
+    get_instituicao.admin_order_field = 'instituicao__instituicao'
+
+    def foi_recebido(self, obj):
+        if obj.recebido:
+            return True
+        else:
+            return False
+
+    foi_recebido.short_description = 'Recebido'
+    foi_recebido.boolean = True
+
+
+admin.site.register(DemandaDoacao, DemandaDoacaoListAdmin)
 admin.site.register(DoacaoUser)
 admin.site.register(DoacaoOutros, DoacaoOutrosListaAdmin)
